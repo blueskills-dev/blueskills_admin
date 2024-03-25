b92acbc9e61592db2b52cdad461b904e1f5b3f6f
eni-fome <fomealalor@gmail.com>
2024-03-25 14:20:03 +0100

    feat: first commmit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('your-login-endpoint-url', {
+      const response = await fetch("https://blueskills3-latest.onrender.com/auth/token/login", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
